56471fea37e7d2b39a7daa39f2999e128e15353f
sheesh3218 <sherylann.wilthien@edu.devinci.fr>
2025-10-02 10:05:03 +0200

    fix: wrong path for home direcrtory

--- scripts/home.nix
+++ scripts/home.nix
@@ -6,30 +6,30 @@
   home.stateVersion = "24.05";
 
   home.file = {
-    ".gitconfig".source = ~/.dotfiles/apps/git/.gitconfig;
-    ".git_func".source = ~/.dotfiles/apps/git/.git_func;
-    ".gitemojis/commit-msg".source = ~/.dotfiles/apps/git/gitemojis/commit-msg;
-    ".gitemojis/prepare-commit-msg".source = ~/.dotfiles/apps/git/gitemojis/prepare-commit-msg;
+    ".gitconfig".source = ./apps/git/.gitconfig;
+    ".git_func".source = ./apps/git/.git_func;
+    ".gitemojis/commit-msg".source = ./apps/git/gitemojis/commit-msg;
+    ".gitemojis/prepare-commit-msg".source = ./apps/git/gitemojis/prepare-commit-msg;
 
-    ".zshrc".source = ~/.dotfiles/apps/zsh/.zshrc;
-    ".zshenv".source = ~/.dotfiles/apps/zsh/.zshenv;
-    ".zprofile".source = ~/.dotfiles/apps/zsh/.zprofile;
-    ".exports".source = ~/.dotfiles/apps/zsh/.exports;
-    ".aliases".source = ~/.dotfiles/apps/zsh/.aliases;
-    ".functions".source = ~/.dotfiles/apps/zsh/.functions;
+    ".zshrc".source = ./apps/zsh/.zshrc;
+    ".zshenv".source = ./apps/zsh/.zshenv;
+    ".zprofile".source = ./apps/zsh/.zprofile;
+    ".exports".source = ./apps/zsh/.exports;
+    ".aliases".source = ./apps/zsh/.aliases;
+    ".functions".source = ./apps/zsh/.functions;
 
-    ".yabairc".source = ~/.dotfiles/apps/yabai/.yabairc;
-    ".skhdrc".source = ~/.dotfiles/apps/yabai/.skhdrc;
-    ".yabai_func".source = ~/.dotfiles/apps/yabai/.yabai_func;
+    ".yabairc".source = ./apps/yabai/.yabairc;
+    ".skhdrc".source = ./apps/yabai/.skhdrc;
+    ".yabai_func".source = ./apps/yabai/.yabai_func;
 
-    ".condarc".source = ~/.dotfiles/apps/conda/.condarc;
-    ".conda_func".source = ~/.dotfiles/apps/conda/.conda_func;
+    ".condarc".source = ./apps/conda/.condarc;
+    ".conda_func".source = ./apps/conda/.conda_func;
 
-    "Library/Application Support/Code/User/settings.json".source = ~/.dotfiles/apps/vscode/settings.json;
-    "Library/Application Support/Code/User/keybindings.json".source = ~/.dotfiles/apps/vscode/keybindings.json;
+    "Library/Application Support/Code/User/settings.json".source = ./apps/vscode/settings.json;
+    "Library/Application Support/Code/User/keybindings.json".source = ./apps/vscode/keybindings.json;
 
-    ".hushlogin".source = ~/.dotfiles/docs/assets/.hushlogin;
-    ".curlrc".source = ~/.dotfiles/apps/curl/.curlrc;
+    ".hushlogin".source = ./docs/assets/.hushlogin;
+    ".curlrc".source = ./apps/curl/.curlrc;
   };
   programs.home-manager.enable = true;
 }
